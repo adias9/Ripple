6ee76f35e62f9e54adc9a99bd994f7cac5a2feaa
Richard Chu <richardchu10@gmail.com>
2016-05-02 01:40:55 -0400

    Add channel system

--- public/main.js
+++ public/main.js
@@ -1,40 +1,112 @@
-// CREATE A REFERENCE TO FIREBASE
-var messagesRef = new Firebase('https://burning-fire-8160.firebaseio.com/');
+// Create a reference to Firebase
+var channelsRef = new Firebase('https://burning-fire-8160.firebaseio.com/');
+var messagesRef = undefined;
 
-// REGISTER DOM ELEMENTS
+// Current channel
+var currentChannel = undefined;
+
+// Register DOM elements
 var messageField = $('#messageInput');
 var nameField = $('#nameInput');
-var messageList = $('#example-messages');
+var messageCol = $('#message-column');
+var addChannelBtn = $('#add-channel-btn');
 
-// LISTEN FOR KEYPRESS EVENT
+var messageList = $('#example-messages');
+var channelList = $('#channels');
+
+function addMessageCallback() {
+  // Add a callback that is triggered for each chat message.
+  messagesRef.on('child_added', function(snapshot) {
+    // Get data
+    var data = snapshot.val();
+    if (data.text === undefined) return;
+
+    var username = data.name || "Anonymous";
+    var message = data.text;
+
+    // Create elements and sanitize text
+    var messageElement = $("<li>");
+    var nameElement = $("<strong class='example-chat-username'></strong>");
+    nameElement.text(username);
+    messageElement.text(message).prepend(nameElement);
+
+    // Add message
+    messageList.append(messageElement);
+
+    // Scroll to bottom of message list
+    messageCol.scrollTop(messageCol[0].scrollHeight);
+  });
+}
+
+// Listen for keypress event
 messageField.keypress(function(e) {
   if (e.keyCode == 13) {
-    //FIELD VALUES
+    // Field values
     var username = nameField.val();
     var message = messageField.val();
 
-    //SAVE DATA TO FIREBASE AND EMPTY FIELD
-    messagesRef.push({ name: username, text: message });
-    messageField.val('');
+    // Save data to Firebase and empty field
+    if (message !== '') {
+      messagesRef.push({ name: username, text: message });
+      messageField.val('');
+    }
   }
 });
 
-// Add a callback that is triggered for each chat message.
-messagesRef.on('child_added', function(snapshot) {
-  //GET DATA
+// Add a callback that is triggered for each channel.
+channelsRef.on('child_added', function(snapshot) {
+  // Get data
   var data = snapshot.val();
-  var username = data.name || "anonymous";
-  var message = data.text;
+  var channelName = data.channelName;
 
-  //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-  var messageElement = $("<li>");
-  var nameElement = $("<strong class='example-chat-username'></strong>")
-  nameElement.text(username);
-  messageElement.text(message).prepend(nameElement);
+  if (currentChannel === undefined) {
+    currentChannel = channelName;
+    messagesRef = snapshot.ref();
+    addMessageCallback();
+  }
 
-  //ADD MESSAGE
-  messageList.append(messageElement)
+  // Create element and sanitize text
+  var channelElement = $("<li class='channel-li'>");
+  channelElement.text(channelName);
 
-  //SCROLL TO BOTTOM OF MESSAGE LIST
-  messageList[0].scrollTop = messageList[0].scrollHeight;
+  // Add message
+  channelList.append(channelElement);
+});
+
+addChannelBtn.click(function() {
+  swal({
+    title: 'Enter a channel name:',
+    input: 'text',
+    showCancelButton: true,
+    inputValidator: function(value) {
+      return new Promise(function(resolve, reject) {
+        if (value) {
+          resolve();
+        } else {
+          reject('The channel name cannot be blank.');
+        }
+      });
+    }
+  }).then(function(result) {
+    if (result) {
+      swal({
+        type: 'success',
+        html: 'You have successfully created the channel!',
+        timer: 1000
+      });
+      messagesRef = channelsRef.push({ channelName: result });
+      messageList.empty();
+      addMessageCallback();
+    }
+  });
+});
+
+channelList.delegate('li', 'click', function() {
+  currentChannel = $(this).text();
+  messageList.empty();
+  channelsRef.orderByChild('channelName').equalTo(currentChannel).on("child_added", function(snapshot) {
+    messagesRef = snapshot.ref();
+    messagesRef.off("child_added");
+    addMessageCallback();
+  });
 });
